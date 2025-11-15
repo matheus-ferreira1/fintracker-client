26b910b59d26de55f04aa8780bc57698a35cac4f
matheus-ferreira1 <matheus.fer33@gmail.com>
2025-11-15 10:37:19 -0300

    feat: added auth layout and routes

---
+++ app/pages/login.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import * as z from 'zod'
+import type { FormSubmitEvent } from '@nuxt/ui'
+
+definePageMeta({
+  layout: 'auth'
+})
+
+useSeoMeta({
+  title: 'Login',
+  description: 'Login to your account to continue'
+})
+
+// const toast = useToast();
+
+const fields = [
+  {
+    name: 'email',
+    type: 'text' as const,
+    label: 'Email',
+    placeholder: 'Enter your email',
+    required: true
+  },
+  {
+    name: 'password',
+    label: 'Password',
+    type: 'password' as const,
+    placeholder: 'Enter your password'
+  }
+]
+
+const schema = z.object({
+  email: z.email('Invalid email'),
+  password: z.string().min(8, 'Must be at least 8 characters')
+})
+
+type Schema = z.output<typeof schema>
+
+function onSubmit(payload: FormSubmitEvent<Schema>) {
+  console.log('Submitted', payload)
+}
+</script>
+
+<template>
+  <UAuthForm
+    :fields="fields"
+    :schema="schema"
+    title="Welcome back"
+    icon="i-lucide-lock"
+    @submit="onSubmit"
+  >
+    <template #description>
+      Don't have an account?
+      <ULink
+        to="/register"
+        class="text-primary font-medium"
+      >Sign up</ULink>.
+    </template>
+
+    <template #password-hint>
+      <ULink
+        to="/"
+        class="text-primary font-medium"
+        tabindex="-1"
+      >Forgot password?</ULink>
+    </template>
+  </UAuthForm>
+</template>
---
+++ app/pages/register.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import * as z from 'zod'
+import type { FormSubmitEvent } from '@nuxt/ui'
+
+definePageMeta({
+  layout: 'auth'
+})
+
+useSeoMeta({
+  title: 'Sign up',
+  description: 'Create an account to get started'
+})
+
+// const toast = useToast();
+
+const fields = [
+  {
+    name: 'name',
+    type: 'text' as const,
+    label: 'Name',
+    placeholder: 'Enter your name'
+  },
+  {
+    name: 'email',
+    type: 'text' as const,
+    label: 'Email',
+    placeholder: 'Enter your email'
+  },
+  {
+    name: 'password',
+    label: 'Password',
+    type: 'password' as const,
+    placeholder: 'Enter your password'
+  }
+]
+
+const schema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  email: z.string().email('Invalid email'),
+  password: z.string().min(8, 'Must be at least 8 characters')
+})
+
+type Schema = z.output<typeof schema>
+
+function onSubmit(payload: FormSubmitEvent<Schema>) {
+  console.log('Submitted', payload)
+}
+</script>
+
+<template>
+  <UAuthForm
+    :fields="fields"
+    :schema="schema"
+    title="Create an account"
+    :submit="{ label: 'Create account' }"
+    @submit="onSubmit"
+  >
+    <template #description>
+      Already have an account?
+      <ULink
+        to="/login"
+        class="text-primary font-medium"
+      >Login</ULink>.
+    </template>
+  </UAuthForm>
+</template>
