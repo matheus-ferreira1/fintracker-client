5cd0c04cd16c08af67e0533cbcac1535a6f9e7c4
matheus-ferreira1 <matheus.fer33@gmail.com>
2025-11-15 10:23:25 -0300

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,29 @@
+<script setup>
+useHead({
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [{ rel: 'icon', href: '/favicon.ico' }],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'FinTracker'
+const description = 'Track your finances with FinTracker'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description
+})
+</script>
+
+<template>
+  <UApp>
+    <NuxtLoadingIndicator />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
