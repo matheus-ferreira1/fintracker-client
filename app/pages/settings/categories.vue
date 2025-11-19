615beac832e765c4985f35e582123f8da319d644
matheus-ferreira1 <matheus.fer33@gmail.com>
2025-11-19 16:55:14 -0300

    feat: added the settings pages

--- app/pages/settings/categories.vue
+++ app/pages/settings/categories.vue
@@ -1,9 +1,32 @@
-<template>
-  <div>
-    categories
-  </div>
-</template>
-
 <script setup lang="ts">
-
+const items = [
+  {
+    label: 'Income',
+    icon: 'material-symbols:trending-up',
+    slot: 'income'
+  },
+  {
+    label: 'Expenses',
+    icon: 'material-symbols:trending-down',
+    slot: 'expenses'
+  }
+]
 </script>
+
+<template>
+  <UPageCard
+    title="Categories"
+    description="Create and customize categories to organize your transactions exactly how you want."
+    variant="subtle"
+  >
+    <UTabs :items="items">
+      <template #income>
+        <CategoriesIncomeList />
+      </template>
+
+      <template #expenses>
+        <CategoriesExpensesList />
+      </template>
+    </UTabs>
+  </UPageCard>
+</template>
--- app/pages/settings/index.vue
+++ app/pages/settings/index.vue
@@ -1,9 +1,100 @@
-<template>
-  <div>
-    settings
-  </div>
-</template>
-
 <script setup lang="ts">
+import * as z from 'zod'
+import type { FormSubmitEvent } from '@nuxt/ui'
 
+const profileSchema = z.object({
+  name: z.string().min(2, 'Too short'),
+  email: z.email('Invalid email'),
+  username: z.string().min(2, 'Too short')
+})
+
+type ProfileSchema = z.output<typeof profileSchema>
+
+const profile = reactive<Partial<ProfileSchema>>({
+  name: 'Benjamin Canac',
+  email: 'ben@nuxtlabs.com',
+  username: 'benjamincanac'
+})
+const toast = useToast()
+async function onSubmit(event: FormSubmitEvent<ProfileSchema>) {
+  toast.add({
+    title: 'Success',
+    description: 'Your settings have been updated.',
+    icon: 'i-lucide-check',
+    color: 'success'
+  })
+  console.log(event.data)
+}
 </script>
+
+<template>
+  <UForm
+    id="settings"
+    :schema="profileSchema"
+    :state="profile"
+    @submit="onSubmit"
+  >
+    <UPageCard
+      title="Profile"
+      description="These informations will be displayed publicly."
+      variant="naked"
+      orientation="horizontal"
+      class="mb-4"
+    >
+      <UButton
+        form="settings"
+        label="Save changes"
+        color="neutral"
+        type="submit"
+        class="w-fit lg:ms-auto"
+      />
+    </UPageCard>
+
+    <UPageCard variant="subtle">
+      <UFormField
+        name="name"
+        label="Name"
+        description="Will appear on receipts, invoices, and other communication."
+        required
+        class="flex max-sm:flex-col justify-between items-start gap-4"
+      >
+        <UInput
+          v-model="profile.name"
+          autocomplete="off"
+        />
+      </UFormField>
+
+      <USeparator />
+
+      <UFormField
+        name="email"
+        label="Email"
+        description="Used to sign in, for email receipts and product updates."
+        required
+        class="flex max-sm:flex-col justify-between items-start gap-4"
+      >
+        <UInput
+          v-model="profile.email"
+          type="email"
+          autocomplete="off"
+        />
+      </UFormField>
+
+      <USeparator />
+
+      <UFormField
+        name="username"
+        label="Username"
+        description="Your unique username for logging in and your profile URL."
+        required
+        class="flex max-sm:flex-col justify-between items-start gap-4"
+      >
+        <UInput
+          v-model="profile.username"
+          type="username"
+          autocomplete="off"
+        />
+      </UFormField>
+    </UPageCard>
+  </UForm>
+</template>
